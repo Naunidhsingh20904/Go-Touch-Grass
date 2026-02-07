e8364e58ddd9571d4b5e73d6eca04df225b2c3a7
Naunidh Singh <naunidhw09@gmail.com>
2026-02-06 20:55:14 -0500

    added settings page

--- app/src/main/java/com/example/gotouchgrass/MainActivity.kt
+++ app/src/main/java/com/example/gotouchgrass/MainActivity.kt
@@ -80,7 +80,7 @@ enum class AppDestinations(
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Gotouchgrass",
         modifier = modifier
     )
 }
